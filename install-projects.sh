ef030d56d7363693dad54c2da2463f1f212ff4ba
Lynchmaniac <lynchmaniac@gmail.com>
2018-10-28 22:50:23 +0100

    feat: add auto clone for dependency project

---
+++ install-projects.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER='lynchmaniac'
+REPO_BROKER='./lea-broker'
+REPO_UI='./lea-ui'
+REPO_TWITTER='./lea-twitter'
+
+GIT=`which git`
+cd ..
+
+# Only proceed if we have a valid repo.
+if [ ! -d ${REPO_BROKER}/.git ]; then
+  echo "${REPO_BROKER} is not install! Installing..." 
+  ${GIT} clone https://github.com/naoned-makers/lea-broker.git
+  echo "${REPO_BROKER} is install"
+  cd ${REPO_BROKER}
+  npm install
+else
+  echo "${REPO_BROKER} is an install repo! Nothing to do..." 
+fi
+
+cd ..
+
+# Only proceed if we have a valid repo.
+if [ ! -d ${REPO_UI}/.git ]; then
+  echo "${REPO_UI} is not install! Installing..." 
+  ${GIT} clone https://github.com/naoned-makers/lea-ui.git
+  echo "${REPO_UI} is install" 
+  cd ${REPO_UI}
+  npm install
+else
+  echo "${REPO_UI} is an install repo! Nothing to do..." 
+fi
+
+cd ..
+
+# Only proceed if we have a valid repo.
+if [ ! -d ${REPO_TWITTER}/.git ]; then
+  echo "${REPO_TWITTER} is not install! Installing..." 
+  ${GIT} clone https://github.com/naoned-makers/lea-twitter.git
+  echo "${REPO_TWITTER} is install" 
+  cd ${REPO_TWITTER}
+  npm install
+else
+  echo "${REPO_TWITTER} is an install repo! Nothing to do..." 
+fi
